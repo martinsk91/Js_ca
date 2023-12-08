48318ba5ab4166aec95830b78eac959b5e942ce2
martinsk91 <132287423+martinsk91@users.noreply.github.com>
2023-12-08 13:25:42 +0100

    CHANGES

---
+++ js/index2.js
@@ -0,0 +1,108 @@
+import { callApi } from "./api.js";
+import {jacket} from "./api.js"
+
+
+
+const button1 = document.querySelector(".bt1");
+const button2 = document.querySelector(".bt2");
+const button3 = document.querySelector(".bt3");
+const button4 = document.querySelector(".bt4");
+const saleContent = document.querySelector(".sale-cont");
+const cross = document.querySelector(".cross");
+const hidden = document.querySelector(".jackets-onsale");
+const first = "first";
+const second = "second";
+const third = "third";
+const fourth = "fourth";
+
+
+
+
+function createContent(){
+  
+
+    
+    const onSale = document.querySelector(".theJackets")
+  ;
+    onSale.innerHTML = ``;
+ 
+  
+    for( let i = 0; i < jacket.length; i++){
+        let title = jacket[i].title;
+        title = title.replace("Rainy Days ", "") 
+       
+        onSale.innerHTML += `<div class="jackets1">
+        
+        <img src="${jacket[i].image}">
+  
+  <p>${title}</p> 
+  <p>${jacket[i].price}</p>
+  <a href="specific.html?id=${jacket[i].id}" class="buyjacket">view more</a>
+  </div>`
+
+if(i ===3){break;}
+}
+
+
+
+
+}
+
+
+createContent();
+
+
+
+function showContent(){
+    hidden.classList.remove("hidden");
+    console.log("yo");
+}
+
+function closeContent(){
+
+  
+    hidden.classList.add("hidden");
+    
+    }
+
+
+function placeContent(add, remove1, remove2, remove3){
+    hidden.classList.add(add);
+    hidden.classList.remove(remove1);
+    hidden.classList.remove(remove2);
+    hidden.classList.remove(remove3);
+    console.log("hello");
+}
+
+
+
+
+
+button1.addEventListener('click', function(){
+    showContent();
+    placeContent(first, second, third, fourth)
+}); 
+
+
+
+button2.addEventListener('click', function(){
+    showContent();
+    placeContent(second, first, third, fourth)
+}); 
+
+button3.addEventListener('click', function(){
+    showContent();
+    placeContent(third, first, second, fourth)
+}); 
+
+button4.addEventListener('click', function(){
+    showContent();
+    placeContent(fourth, first, second, third)
+}); 
+
+cross.addEventListener('click', closeContent);
+
+
+
+
+
